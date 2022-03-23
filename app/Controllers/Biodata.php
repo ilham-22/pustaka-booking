bc963319e5165821cb79189a3a98a783e8a2bcc9
ilham220500 <ilham220500@gmail.com>
2022-03-24 04:21:50 +0700

    source code

---
+++ app/Controllers/Biodata.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+class biodata extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya ilham 
+            saya tinggal di daerah jakarta barat 
+            hobi saya adalah biliard 
+            saya anak kedua dari empat bersaudara";
+
+
+    }
+}
